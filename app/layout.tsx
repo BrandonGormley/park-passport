3ed9649966b972553a1bf07d66618270eb134d59
Brandon Gormley <brandontgormley@gmail.com>
2023-05-03 11:50:47 -0500

    update meta title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import { Inter } from 'next/font/google';
+import { Header } from './components/Header/Header';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
-    title: 'Park Passport',
+    title: 'Park Passport - Start exploring today!',
     description:
         'Discover national parks before you go with Park Passport, a web app built using NextJS, React, and other modern technologies. Retrieve important park information, upvote or like your favorite parks, and access it all in one place. Start planning your next adventure today!',
 };
@@ -18,6 +19,7 @@ export default function RootLayout({
     return (
         <html lang='en'>
             <body className={inter.className}>
+                <Header />
                 {children}
                 <Analytics />
             </body>
