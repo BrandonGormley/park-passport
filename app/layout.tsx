97dbbbac98d0dd973a1d4d74f4ba41d7ffea4bde
Brandon Gormley <brandontgormley@gmail.com>
2023-05-05 15:19:17 -0500

    add Footer component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import { Analytics } from '@vercel/analytics/react';
 import './globals.css';
 import { Inter } from 'next/font/google';
 import { Header } from './components/Header/Header';
+import { Footer } from './components/Footer/Footer';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,6 +24,7 @@ export default function RootLayout({
             >
                 <Header />
                 {children}
+                <Footer />
                 <Analytics />
             </body>
         </html>
