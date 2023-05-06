ae90c0c873a1e91385efadaab62c75f5e62de0c1
Brandon Gormley <brandontgormley@gmail.com>
2023-05-06 12:07:03 -0500

    add new activities card to component

--- app/components/SearchParks/SearchParks.tsx
+++ app/components/SearchParks/SearchParks.tsx
@@ -3,11 +3,17 @@ import { useState } from 'react';
 import { TiWeatherPartlySunny } from 'react-icons/ti';
 import { HiOutlineNewspaper, HiOutlineCurrencyDollar } from 'react-icons/hi';
 import { GrMapLocation } from 'react-icons/gr';
+import { FaWalking } from 'react-icons/fa';
 import Link from 'next/link';
 
 type EntranceFee = {
     cost: String;
     title: String;
+};
+
+type Activity = {
+    id: String;
+    name: String;
 };
 
 type Park = {
@@ -16,6 +22,7 @@ type Park = {
     weatherInfo: String;
     directionsUrl: String;
     entranceFees: EntranceFee[];
+    activities: Activity[];
 };
 
 export const SearchParks = () => {
@@ -43,7 +50,7 @@ export const SearchParks = () => {
         <section className='my-12 flex flex-col items-center justify-center p-4'>
             <input
                 type='text'
-                className='w-3/4 max-w-[400px] rounded-lg bg-white px-3 py-2 text-center text-gray-800 outline-none transition-all duration-300 focus:shadow-md md:w-1/2'
+                className='w-3/4 max-w-[400px] rounded-lg bg-white px-3 py-2 text-center text-gray-800 shadow outline-none transition-all duration-300 focus:shadow-lg md:w-1/2'
                 placeholder='Park Code ex: GRCA'
                 value={query}
                 onChange={handleInputChange}
@@ -63,8 +70,8 @@ export const SearchParks = () => {
                     <h2 className='mb-2 text-center text-3xl font-bold text-gray-700'>
                         {activePark.fullName}
                     </h2>
-                    <div className='flex w-full flex-col flex-wrap md:flex-row'>
-                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:mb-0 md:w-[45%]'>
+                    <div className='flex w-full flex-col flex-wrap justify-around md:flex-row'>
+                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:w-[45%]'>
                             <div className='mb-2 flex flex-row items-center justify-center border-b border-b-gray-200 pb-2'>
                                 <HiOutlineNewspaper className='h-8 w-8 text-gray-700' />
                                 <h3 className='ml-2 text-lg'>Description</h3>
@@ -73,7 +80,7 @@ export const SearchParks = () => {
                                 {activePark.description}
                             </p>
                         </div>
-                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:mb-0 md:w-[45%]'>
+                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:w-[45%]'>
                             <div className='mb-2 flex flex-row items-center justify-center border-b border-b-gray-200 pb-2'>
                                 <TiWeatherPartlySunny className='h-8 w-8 text-gray-700' />
                                 <h3 className='ml-2 text-lg'>Weather</h3>
@@ -83,7 +90,7 @@ export const SearchParks = () => {
                                 {activePark.weatherInfo}
                             </p>
                         </div>
-                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:mb-0 md:w-[45%]'>
+                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:w-[45%]'>
                             <div className='mb-2 flex flex-row items-center justify-center border-b border-b-gray-200 pb-2'>
                                 <HiOutlineCurrencyDollar className='h-8 w-8 text-gray-700' />
                                 <h3 className='ml-2 text-lg'>Entrance Fees</h3>
@@ -104,7 +111,7 @@ export const SearchParks = () => {
                                 );
                             })}
                         </div>
-                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:mb-0 md:w-[45%]'>
+                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:w-[45%]'>
                             <div className='mb-2 flex flex-row items-center justify-center border-b border-b-gray-200 pb-2'>
                                 <GrMapLocation className='h-8 w-8 text-gray-700' />
                                 <h3 className='ml-2 text-lg'>Directions</h3>
@@ -117,6 +124,23 @@ export const SearchParks = () => {
                             >
                                 Click for directions
                             </Link>
+                        </div>
+                        <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4'>
+                            <div className='mb-2 flex flex-row items-center justify-center border-b border-b-gray-200 pb-2'>
+                                <FaWalking className='h-8 w-8 text-gray-700' />
+                                <h3 className='ml-2 text-lg'>Activities</h3>
+                            </div>
+
+                            <p>
+                                {activePark.activities.map((activity) => {
+                                    return (
+                                        // @ts-ignore
+                                        <span key={activity.id}>
+                                            {activity.name},{' '}
+                                        </span>
+                                    );
+                                })}
+                            </p>
                         </div>
                     </div>
                 </div>
