30eebc794d06d7b990e8363cb2a9c151228a4c58
Brandon Gormley <brandontgormley@gmail.com>
2023-05-08 08:55:08 -0500

    remove inter import from page level

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,4 @@
-import { Inter } from 'next/font/google';
 import { SearchParks } from './components/SearchParks/SearchParks';
-
-const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
     return (
