3cf81d3a9b0acdb575ab13d3475a85a966be77d2
Brandon Gormley <brandontgormley@gmail.com>
2023-05-10 12:20:34 -0500

    add Image to app

--- app/components/Park/ParkDetails.tsx
+++ app/components/Park/ParkDetails.tsx
@@ -4,6 +4,7 @@ import { GrMapLocation } from 'react-icons/gr';
 import { FaWalking } from 'react-icons/fa';
 import { Park } from '@/app/types/Park/Park';
 import Link from 'next/link';
+import Image from 'next/image';
 
 type ParkDetailsProps = {
     park: Park;
@@ -15,6 +16,17 @@ export const ParkDetails: React.FC<ParkDetailsProps> = ({ park }) => {
             <h2 className='mb-2 text-center text-3xl font-bold text-gray-700'>
                 {park.fullName}
             </h2>
+            <Image
+                src={`${park.images[0].url}`}
+                width={390}
+                height={260}
+                alt={`${park.images[0].altText}`}
+                className='m-4 rounded-lg'
+            />
+            <p className='my-2'>
+                Credit:{' '}
+                <span className='font-bold'>{park.images[0].credit}</span>
+            </p>
             <div className='flex w-full flex-col flex-wrap justify-around md:flex-row'>
                 <div className='mb-4 flex w-full flex-col rounded-md bg-white p-4 shadow-md shadow-slate-300 md:mx-4 md:my-4 md:w-[45%]'>
                     <div className='mb-2 flex flex-row items-center justify-center border-b border-b-gray-200 pb-2'>
