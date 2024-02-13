b4c5458cb7ee7b825976f7b4fa7009403bcde133
Brandon Gormley <brandontgormley@gmail.com>
2024-02-13 11:34:10 -0600

    add dnyamic route to list states

---
+++ src/app/parks/[code]/page.tsx
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+
+interface ParkStateProps {
+    params: {
+        code: string;
+    };
+}
+
+export async function getData(code: string) {
+    const res = await fetch(
+        `https://developer.nps.gov/api/v1/parks?stateCode=${code}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+    );
+    const data = await res.json();
+    return data;
+}
+
+export default async function page({ params }: ParkStateProps) {
+    const parkList = await getData(params.code);
+
+    return (
+        <main className='min-h-screen'>
+            <section className='w-full mx-auto px-8 max-w-[1280px] mt-32'>
+                <h1>National Parks for : {params.code.toUpperCase()}</h1>
+                <ul className='my-4'>
+                    {parkList.data.map((park: any) => (
+                        <li className='inline-block' key={park.id}>
+                            <Link
+                                className='px-2 py-1 text-xs rounded-lg bg-slate-100 text-gray-700 inline-block mr-2 my-1 hover:bg-slate-300 transition-all'
+                                href={`/parks/${params.code}/${park.id}`}
+                            >
+                                {park.fullName}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </section>
+        </main>
+    );
+}
