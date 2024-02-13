ac4f712ff22bd98583db25335db1cc6d31386f5c
Brandon Gormley <brandontgormley@gmail.com>
2024-02-13 11:33:53 -0600

    update root styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,26 @@ export default function Home() {
     return (
         <main className='min-h-screen'>
             <section className='w-full mx-auto px-8 max-w-[1280px] mt-32'>
-                <div className='lg:py-8'>
+                <div className='flex flex-col justify-center items-center lg:py-8'>
                     <h1 className='text-3xl font-bold tracking-tighter md:text-4xl lg:text-5xl xl:text-6xl text-gray-800'>
-                        Welcome to Park Passport
+                        Welcome to Park{' '}
+                        <span className='bg-gradient-to-r from-green-500 via-lime-500 to-green-500 bg-clip-text text-transparent'>
+                            Passport
+                        </span>
                     </h1>
-                    <p className='text-gray-500 my-4 lg:max-w-[60%]'>
+                    <p className='text-gray-500 my-6 lg:max-w-[75%] text-center'>
                         Discover national parks before you go with Park
                         Passport. Retrieve important park information, upvote or
                         like your favorite parks, and access it all in one
                         place. Start planning your next adventure today!
                     </p>
+                    <Link
+                        className='bg-gray-800 text-white rounded-lg px-6 py-3 inline-block text-center hover:bg-gray-700 transition-all mx-auto'
+                        href='/parks'
+                    >
+                        Check out parks today
+                    </Link>
                 </div>
-                <Link
-                    className='bg-gray-800 text-white rounded-lg px-6 py-3 block text-center md:inline hover:bg-gray-700 transition-all'
-                    href='/blog'
-                >
-                    Check out parks today
-                </Link>
             </section>
         </main>
     );
