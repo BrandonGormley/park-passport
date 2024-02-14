af72b07b99b775800c70a6f2f2e955942d6f8aa4
Brandon Gormley <brandontgormley@gmail.com>
2024-02-14 09:37:17 -0600

    add typing to park detail page

--- src/app/parks/[code]/[id]/page.tsx
+++ src/app/parks/[code]/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { TiWeatherPartlySunny } from 'react-icons/ti';
+import { Park } from '@/app/lib/types';
 
 export const dynamic = 'force-dynamic';
 
@@ -14,18 +15,19 @@ async function getData(code: string, id: string) {
         `https://developer.nps.gov/api/v1/parks?stateCode=${code}&q=${id}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
     );
     const data = await res.json();
-    return data;
+    return data.data;
 }
 
 export default async function page({ params }: ParkDetailProps) {
-    const { data } = await getData(params.code, params.id);
+    const data = await getData(params.code, params.id);
+    const park: Park = data[0];
     return (
         <main className='min-h-screen'>
             <section className='w-full mx-auto px-8 max-w-[1280px] mt-32'>
                 <h1 className='text-lg font-bold tracking-tighter md:text-xl lg:text-2xl xl:text-3xl text-gray-800'>
-                    {data[0].fullName}
+                    {park.fullName}
                 </h1>
-                <p className='text-gray-700 my-4'>{data[0].description}</p>
+                <p className='text-gray-700 my-4'>{park.description}</p>
                 <p className='text-gray-700 my-4 flex flex-col'>
                     <div className='flex flex-row'>
                         <TiWeatherPartlySunny className='w-6 h-6 mb-1' />{' '}
@@ -33,13 +35,13 @@ export default async function page({ params }: ParkDetailProps) {
                     </div>
                     <span className='text-gray-600 text-xs pl-2'>
                         {' '}
-                        {data[0].weatherInfo}
+                        {park.weatherInfo}
                     </span>
                 </p>
                 <a
                     target='_blank'
                     className='my-2 text-gray-500 hover:text-gray-700 transition-colors text-sm hover:border-b border-gray-400'
-                    href={data[0].directionsUrl}
+                    href={park.directionsUrl}
                 >
                     For more details &#8250;
                 </a>
