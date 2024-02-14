693d62f1559b9950231b9476894600b62a62fe82
Brandon Gormley <brandontgormley@gmail.com>
2024-02-14 09:34:35 -0600

    add correct typing

--- src/app/parks/[code]/page.tsx
+++ src/app/parks/[code]/page.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import { Park } from '@/app/lib/types';
 
 export const dynamic = 'force-dynamic';
 
@@ -13,18 +14,18 @@ async function getData(code: string) {
         `https://developer.nps.gov/api/v1/parks?stateCode=${code}&api_key=${process.env.NEXT_PUBLIC_API_KEY}`
     );
     const data = await res.json();
-    return data;
+    return data.data;
 }
 
 export default async function page({ params }: ParkStateProps) {
-    const parkList = await getData(params.code);
+    const data: Park[] = await getData(params.code);
 
     return (
         <main className='min-h-screen'>
             <section className='w-full mx-auto px-8 max-w-[1280px] mt-32'>
                 <h1>National Parks for : {params.code.toUpperCase()}</h1>
                 <ul className='my-4'>
-                    {parkList.data.map((park: any) => (
+                    {data.map((park: Park) => (
                         <li className='inline-block' key={park.id}>
                             <Link
                                 className='px-2 py-1 text-xs rounded-lg bg-slate-100 text-gray-700 inline-block mr-2 my-1 hover:bg-slate-300 transition-all'
