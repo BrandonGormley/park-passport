c9e6acb35e3a4ecceb7e67016dd58408b94c8b0b
Brandon Gormley <brandontgormley@gmail.com>
2024-02-15 12:03:31 -0600

    remove jsx and convert to individual components

--- src/app/parks/[code]/[id]/page.tsx
+++ src/app/parks/[code]/[id]/page.tsx
@@ -1,6 +1,10 @@
 import { Park } from '@/app/lib/types';
-import Image from 'next/image';
-import { ParkImage } from '@/app/lib/types';
+import ParkName from '@/app/components/ui/park/ParkName';
+import ParkCoordinates from '@/app/components/ui/park/ParkCoordinates';
+import ParkDescription from '@/app/components/ui/park/ParkDescription';
+import ParkWeather from '@/app/components/ui/park/ParkWeather';
+import ParkDirections from '@/app/components/ui/park/ParkDirections';
+import ParkImages from '@/app/components/ui/park/ParkImages';
 
 export const dynamic = 'force-dynamic';
 
@@ -25,44 +29,19 @@ export default async function page({ params }: ParkDetailProps) {
     return (
         <main className='min-h-screen'>
             <section className='w-full mx-auto px-8 max-w-[1280px] mt-32'>
-                <h1 className='text-lg font-bold tracking-tighter md:text-xl lg:text-2xl xl:text-3xl text-gray-800'>
-                    {park.fullName}{' '}
-                    <span className='text-gray-400 font-normal text-sm tracking-normal ml-4'>
-                        {park.states}
-                    </span>
-                </h1>
-                <p className='text-gray-400 text-xs mt-2'>
-                    {park.latitude},{park.longitude}
-                </p>
+                <ParkName parkName={park.fullName} parkStates={park.states} />
+                <ParkCoordinates
+                    parkLongitude={park.longitude}
+                    parkLatitude={park.latitude}
+                />
 
-                <p className='text-gray-700 my-4'>{park.description}</p>
-                <p className='flex flex-row font-semibold my-2'>Weather:</p>
-                <p className='text-gray-600 text-xs mt-2'>{park.weatherInfo}</p>
-                <p className='flex flex-row font-semibold my-2'>Drections:</p>
-                <p className='text-gray-600 text-xs mt-2'>
-                    {park.directionsInfo}
-                </p>
-
-                <a
-                    target='_blank'
-                    className='my-4 text-gray-500 hover:text-gray-700 transition-colors text-xs hover:border-b border-gray-400'
-                    href={park.directionsUrl}
-                >
-                    For more details &#8250;
-                </a>
-                <div className='flex flex-wrap m-4 ml-0'>
-                    {park.images.map((image: ParkImage) => (
-                        <Image
-                            className='m-2 rounded-lg'
-                            key={image.title}
-                            src={image.url}
-                            alt={image.altText}
-                            height={100}
-                            width={100}
-                            quality={100}
-                        />
-                    ))}
-                </div>
+                <ParkDescription parkDescription={park.description} />
+                <ParkWeather parkWeatherInfo={park.weatherInfo} />
+                <ParkDirections
+                    parkDirectionsInfo={park.directionsInfo}
+                    parkDirectionsUrl={park.directionsUrl}
+                />
+                <ParkImages parkImages={park.images} />
             </section>
         </main>
     );
