39a4e3c0e7f3fcdc01ecdfb587e1b0e8a0976ba2
Brandon Gormley <brandontgormley@gmail.com>
2024-02-15 11:41:02 -0600

    update root meta data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,12 +10,9 @@ export const metadata: Metadata = {
     description:
         'Discover national parks before you go with Park Passport, a web app built using NextJS, React, and other modern technologies. Retrieve important park information, upvote or like your favorite parks, and access it all in one place. Start planning your next adventure today!',
     openGraph: {
-        // images: [
-        //     {
-        //         url: 'https://park-passport.vercel.app/banner.jpg',
-        //     },
-        // ],
-        locale: 'en-US',
+        url: 'https://park-passport.vercel.app',
+        siteName: 'Park Passport',
+        locale: 'en_US',
         type: 'website',
     },
 };
