01d4c5526170c4c162282b5a2e55851c377b0c53
Brandon Gormley <brandontgormley@gmail.com>
2024-02-15 12:04:10 -0600

    create ParkImages component

---
+++ src/app/components/ui/park/ParkImages.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+import { ParkImage } from '@/app/lib/types';
+
+interface ParkImageProps {
+    parkImages: ParkImage[];
+}
+
+export default function ParkImages({ parkImages }: ParkImageProps) {
+    return (
+        <>
+            <div className='flex flex-wrap m-4 ml-0'>
+                {parkImages.map((image: ParkImage) => (
+                    <Image
+                        className='m-2 rounded-lg'
+                        key={image.title}
+                        src={image.url}
+                        alt={image.altText}
+                        height={100}
+                        width={100}
+                        quality={100}
+                    />
+                ))}
+            </div>
+        </>
+    );
+}
