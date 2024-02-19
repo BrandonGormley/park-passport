aaaf46f140781bd525dc83ed0d5e36dc03bc97c3
Brandon Gormley <brandontgormley@gmail.com>
2024-02-19 11:56:26 -0600

    add key to .map rendering

--- src/app/parks/[code]/[id]/page.tsx
+++ src/app/parks/[code]/[id]/page.tsx
@@ -50,6 +50,7 @@ export default async function page({ params }: ParkDetailProps) {
                     {park.entranceFees.length > 0 ? (
                         park.entranceFees.map((entranceFee) => (
                             <ParkEntranceFee
+                                key={entranceFee.title}
                                 title={entranceFee.title}
                                 cost={entranceFee.cost}
                                 description={entranceFee.description}
