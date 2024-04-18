c22aba3afea75c756f316b757e863e9c0b8e0f30
Brandon Gormley <brandontgormley@gmail.com>
2024-04-18 13:30:20 -0500

    park hours are not rendering correctly

--- src/app/parks/[code]/[id]/page.tsx
+++ src/app/parks/[code]/[id]/page.tsx
@@ -86,6 +86,7 @@ export default async function page({ params }: ParkDetailProps) {
                         Email: {park.contacts.emailAddresses[0].emailAddress}
                     </p>
                 </>
+                {/* *BUG* Hours are not rendering as they should */}
                 {/* <ParkHours parkOperatingHours={park.operatingHours} /> */}
                 <ParkEntranceFees parkEntranceFees={park.entranceFees} />
                 <ParkImages parkImages={park.images} />
